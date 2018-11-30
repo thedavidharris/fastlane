10548e785667759ad4aa489abf2316fcd5c89b66
David <daharr@umich.edu>
2018-11-29 19:46:41 -0500

    Catch JSON parsing

--- fastlane/swift/Fastlane.swift
+++ fastlane/swift/Fastlane.swift
@@ -4128,7 +4128,14 @@ func parseDictionaryHelper(fromString: String, function: String = #function) -> 
   } else {
       potentialDictionary = fromString
   }
-  let dictionary: [String : Any] = try! JSONSerialization.jsonObject(with: potentialDictionary.data(using: .utf8)!, options: []) as! [String : Any]
+  let dictionary: [String: Any]
+  do {
+    dictionary = try JSONSerialization.jsonObject(with: potentialDictionary.data(using: .utf8)!, options: []) as! [String : Any]
+  } catch {
+    println(error)
+    dictionary = [String:Any]()
+  }
+    
   return dictionary
 }
 
